07dd3d574fb93f16ba2f2d52b39310661be8a0ed
Luthuos <ecolau2017@gmail.com>
2022-05-26 10:29:30 -0300

    inicio del proyecto Mi Web

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { motion } from "framer-motion"
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
